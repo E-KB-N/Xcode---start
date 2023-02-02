8348c6a5f45ec20884a1f4adfe82e6f903e0b53f
Edward Kojo Boateng Ntow <Kojoboateng514@gmail.com>
2023-02-02 23:53:51 +0000

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Ed
+//
+//  Created by Kojo Boateng on 1/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
